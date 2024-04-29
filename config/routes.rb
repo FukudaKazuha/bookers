ac4f4a763767ebc1eab1b707b23f93a3a7cec58c
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-04-29 06:03:15 +0000

    [Add]ルーテイング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'=>'books#edit'
   get '/top' => 'homes#top'
+  resources :books
+  # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能
+  
+  # top画面は/だけで表示されるようにすること
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
