9bd455aaff183457dea49795bed9c7008df7b83d
EC2 Default User <ec2-user@ip-172-31-36-164.ap-northeast-1.compute.internal>
2024-04-29 09:00:22 +0000

    休憩コミット

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def new
+    #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する
+    @book=Book.new
   end
+  
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to '/show'
+    # リダイレクト先を投稿一覧にする
+  end  
 
   def index
   end
 
   def show
+    
   end
 
   def edit
+  end
+  
+  # プライベートより後に定義されたメソッドは、アクションとして認識されなくなる。つまりURLと対応できなくなる。
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  # get 'books/new'
+  get 'books/new'
+  post 'books' => 'books#create'
   # get 'books/index'
-  # get 'books/show'
+  get '/show'=>'books#show'
   # get 'books/edit'=>'books#edit'
   get '/top' => 'homes#top'
-  resources :books
+  # resources :books
   # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能
   
   # top画面は/だけで表示されるようにすること
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
